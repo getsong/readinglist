ca794ad8a941f031bf46d48baf271a3836029671
work <jiacong.pan@autodesk.com>
2019-11-02 22:58:52 +0800

    Process creation of item in controller

--- src/main/java/com/getsong/readinglist/controllers/ReadingItemController.java
+++ src/main/java/com/getsong/readinglist/controllers/ReadingItemController.java
@@ -1,9 +1,12 @@
 package com.getsong.readinglist.controllers;
 
+import com.getsong.readinglist.daos.Dao;
+import com.getsong.readinglist.models.ReadingItem;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 /**
  * Controller for Reading List Items
@@ -13,12 +16,29 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 @Slf4j
+@RequestMapping("/items")
 public class ReadingItemController {
 
-  @GetMapping("/items")
+  private Dao<ReadingItem> readingItemDao;
+
+  public ReadingItemController(Dao<ReadingItem> readingItemDao) {
+    this.readingItemDao = readingItemDao;
+  }
+
+  @GetMapping
   @ResponseBody
   public String getAllItems() {
     log.info("getting all items");
     return "all items are here";
+  }
+
+  @PostMapping
+  @ResponseBody
+  public String createItem(@RequestBody ReadingItem readingItem) {
+    readingItem.setCreationTime(new Date());
+    readingItemDao.save(readingItem);
+    String message = String.format("creating an item: %s", readingItem);
+    log.info(message);
+    return message;
   }
 }
